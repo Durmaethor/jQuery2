a931772e031e238de739ccbe1a416303ed65bb0a
Ryan Kramer <rkramer613@gmail.com>
2017-01-31 19:53:22 -0700

    clear the form

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -11,10 +11,23 @@ $(document).ready(function() {
       if(task) {
           task = new Task(task);
           listo.push(task);
+
+          $('#newItemInput').val('');
+            $('#newList').append(
+                        '<a href="#finish" class="" id="item">' +
+                        '<li class="list-group-item">' +
+                        '<h3>' + task.task + '</h3>'+
+                        '<span class="arrow pull-right">' +
+                        '<i class="glyphicon glyphicon-arrow-right">' +
+                        '</span>' +
+                        '</li>' +
+                        '</a>'
+                    );
+                    
       }
   };
 
-  
+
 
 
 
